ab9d34220f7c53e68f8bdd03ef755079faa7ddf7
reetika <reetika.singh@byjus.com>
2021-03-26 17:32:59 +0530

    added requisite group page and conditions

---
+++ src/Components/constant.js
@@ -0,0 +1,16 @@
+const setOfConstants = {
+    daysInWeek: [
+        { label: "Monday", value: "Monday" },
+        { label: "Tuesday", value: "Tuesday" },
+        { label: "Wednesday", value: "Wednesday" },
+        { label: "Thursday", value: "Thursday" },
+        { label: "Friday", value: "Friday" },
+        { label: "Saturday", value: "Saturday" },
+        { label: "Sunday", value: "Sunday" }],
+    SessionType: [
+        { label: "Optional", value: 'false' },
+        { label: "Mandatory", value: 'true' }
+    ]
+}
+
+export default setOfConstants;
