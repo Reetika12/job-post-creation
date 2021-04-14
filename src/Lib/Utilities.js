3d03e6538e0c2e7490a8123eae1e4eda87296868
reetika <reetika.singh@byjus.com>
2021-04-14 17:48:36 +0530

    updated url params

--- src/Lib/Utilities.js
+++ src/Lib/Utilities.js
@@ -4,6 +4,7 @@ import AppConfig from '../Config/AppConfig';
 
 export function parseUrl(url) {
     url = url || window.location.href;
+    url = window.decodeURIComponent(url)
     let urlParams = {};
     let hashs = url.split('?');
     hashs.forEach((params) => {
